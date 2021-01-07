8fe417c82848a44e690dd65e2309f0d14286c427
ahmsalah <aemdsalah@gmail.com>
2021-01-07 17:14:30 +0200

    Add constants & reusable sentry config

---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
+export const isIOS =
+  /iPad|iPhone|iPod/.test(navigator.platform) ||
+  (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1 && !window.MSStream);
