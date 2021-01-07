30b2992dcaed11bd342ccce9d9c25632e44fa221
ahmsalah <aemdsalah@gmail.com>
2021-01-07 18:49:15 +0200

    Add API base-url

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,14 +6,16 @@ const api = axios.create({
   baseURL,
 });
 
-/**
- * axios interceptors
- */
-// api.interceptors.request.use(config => {
-// const token = store.getState()?.auth?.currentUser?.token;
-// eslint-disable-next-line no-param-reassign
-// config.headers.Authorization = token || '';
-// return config;
-// });
+/* axios interceptors */
+
+api.interceptors.request.use(config => {
+  return {
+    ...config,
+    params: {
+      ...config.params,
+      source: 'mweb',
+    },
+  };
+});
 
 export default api;
