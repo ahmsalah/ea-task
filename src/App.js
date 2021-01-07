fd3d5c176aa8c5f683d9fbc7821eb447faa4ef87
ahmsalah <aemdsalah@gmail.com>
2021-01-07 17:32:06 +0200

    Connect it all in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from 'theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Snackbar } from 'utils/snackbar';
+import NoConnection from 'utils/NoConnection';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+import ErrorBoundary from 'utils/ErrorBoundary';
 
 function App() {
-  return <div className="App">init</div>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Snackbar>
+          <CssBaseline />
+          <NoConnection />
+          <ErrorBoundary>
+            <h1>init</h1>
+          </ErrorBoundary>
+        </Snackbar>
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
 export default App;
